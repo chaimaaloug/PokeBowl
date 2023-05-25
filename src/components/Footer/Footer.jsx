34be1d6264b02d5fa41ab2837b9e8bdcb261bc99
bore-prog <philippeaurore@orange.fr>
2023-05-25 17:47:48 +0200

    Add newsletter form to footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -4,6 +4,14 @@ const Footer = () => {
     return(
         <footer className={style.footer}>
             <p>Footer</p>
+            <div className={style.newsletter}>
+                <p className={style.newsletterTitle}>Inscrivez-vous à notre newsletter</p>
+                <form action="" method="post" className={style.newsletterForm}>
+                    <label for="mail-newsletter">Votre email</label>
+                    <input type="email" name="mail-newsletter" id="mail-newsletter"/>
+                    <button>S'inscrire</button>
+                </form>
+            </div>
         </footer>
     )
 }
