8d75d36c4bf53997fd0f56de1696ee4f7f82245b
bore-prog <philippeaurore@orange.fr>
2023-05-25 17:10:57 +0200

    Add menu to header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,6 +4,14 @@ const Header = () => {
     return(
         <header className={style.header}>
             <h1 className={style.title}>PokeBowl</h1>
+            <nav>
+                <ul>
+                    <li><a href="/">Accueil</a></li>
+                    <li><a href="/menu">Menu</a></li>
+                    <li><a href="/contact">Contact</a></li>
+                    <li><a href="/panier">Panier</a></li>
+                </ul>
+            </nav>
         </header>
     )
 }
