313fca09b6e0b21ee65fc72b9ce39ab9e3af5d39
Pierre Beuselinck <pierrebeuselinck2@gmail.com>
2023-06-24 16:54:31 +0200

    add layout (scss).

--- src/pages/Error/Error.jsx
+++ src/pages/Error/Error.jsx
@@ -1,14 +1,13 @@
 import { useRouteError } from "react-router-dom";
-import style from "./Error.module.scss";
 import BackButton from "../../components/BackButton/BackButton";
 
 const Error = () => {
     const {status} = useRouteError();
 
     return (
-        <div className={style.container}>
-            <h2 className={style.errorStatus}>{status}</h2>
-            <p className={style.message}>La page que vous recherchez semble introuvable.</p>
+        <div className="container">
+            <h2 className="errorStatus">{status}</h2>
+            <p className="message">La page que vous recherchez semble introuvable.</p>
             <BackButton title="Revenir au site"/>
         </div>
     );
